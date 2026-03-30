666e1d592f05764763f8f89e0e40f1615181b3cc
JhantuBala <jhantu.bala@geotechinfo.net>
2026-03-30 10:31:38 +0530

    added the userToDto to convert the User into User Dto

--- src/main/java/io/app/mapper/ModelMapper.java
+++ src/main/java/io/app/mapper/ModelMapper.java
@@ -1,7 +1,9 @@
 package io.app.mapper;
 
 import io.app.dto.TaskDto;
+import io.app.dto.UserDto;
 import io.app.model.Task;
+import io.app.model.User;
 
 public class ModelMapper {
     public static TaskDto taskToDto(Task task){
@@ -13,5 +15,16 @@ public class ModelMapper {
                 .updatedAt(task.getUpdatedAt())
                 .createdAt(task.getCreatedAt())
                 .build();
+    }
+    public static UserDto userToDto(User user){
+        UserDto userDto=UserDto.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .roles(user.getRoles())
+                .id(user.getId())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+        return userDto;
     }
 }
